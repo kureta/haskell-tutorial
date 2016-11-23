adab37277ec59d56752d3fbe91630e89ccba6eb7
kureta <skureta@gmail.com>
2016-11-23 22:13:23 +0300

    Unicodified rotate function

--- lib/Euclid.hs
+++ lib/Euclid.hs
@@ -5,7 +5,7 @@ module Euclid where
 import           Music.Theory.Bjorklund
 import           Prelude.Unicode
 
-rotate :: Int -> [a] -> [a]
+rotate ∷ Int → [a] → [a]
 rotate _ [] = []
 rotate n xs = zipWith const (drop n (cycle xs)) xs
 
